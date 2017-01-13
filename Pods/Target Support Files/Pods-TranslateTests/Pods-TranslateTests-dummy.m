bc7034b9ba659035b4c5130b5a23941eab22d047
Bryan <bryanye97@gmail.com>
2017-01-13 12:46:40 +1100

    Added Firebase pods

---
+++ Pods/Target Support Files/Pods-TranslateTests/Pods-TranslateTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TranslateTests : NSObject
+@end
+@implementation PodsDummy_Pods_TranslateTests
+@end
---
+++ Pods/Target Support Files/Pods-TranslateUITests/Pods-TranslateUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TranslateUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TranslateUITests
+@end
